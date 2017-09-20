da6d73ab9cc67cb4f8046f7aeb296247ccb0d2b2
trungptvnu <trungptvnu@gmail.com>
2017-09-20 21:04:04 +0700

    Second commit

--- Variable/main.cpp
+++ Variable/main.cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
+class C {
+public:
+    int a;
+    int b;
+private:
+    int c;
+    int d;
+};
+
 int main(int argc, char *argv[])
 {
+    int test;
+    cout << sizeof(test) <<endl;
     cout << "Hello World!" << endl;
+
+    cout << " Test Static avariable" << endl;
+    for (int i=0; i <=5; i++) {
+        int x;
+
+    }
+
     return 0;
 }
