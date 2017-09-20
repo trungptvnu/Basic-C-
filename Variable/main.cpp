ee333e7dc4d78e277c091de06c0333a6096e0f64
trungptvnu <trungptvnu@gmail.com>
2017-09-20 23:16:08 +0700

    Create project for Pointer

--- Variable/main.cpp
+++ Variable/main.cpp
@@ -11,6 +11,19 @@ private:
     int d;
 };
 
+
+void setNum(int a) {
+    a =0;
+}
+void swap(int &a, int &b)
+{
+    int c = a;
+    a =b;
+    b = c;
+    cout << a <<"--"<<b << endl;
+}
+
+
 int main(int argc, char *argv[])
 {
     int test;
@@ -22,6 +35,26 @@ int main(int argc, char *argv[])
         int x;
 
     }
+
+    cout << "Test While" <<endl;
+    int x = 10;
+    do {
+        cout <<" x = " << x << endl;
+        x--;
+    } while (x >0);
+
+    cout << "Test tham bien tham tri " <<endl;
+    int b =2;
+    setNum(b);
+    cout <<" gia tri cua b sau khi goi ham la " << b <<endl;
+
+    // Truyen theo tham bien
+    int m =2;
+    int n =3;
+    cout <<" &xx"<< &n<<endl;
+    swap(m,n);
+
+    cout << "Test Mảng" <<endl;
 
     return 0;
 }
