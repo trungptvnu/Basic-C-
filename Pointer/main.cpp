8f0c488351545434cdbc88a8e3a8c6d32bccec7b
trungptvnu <trungptvnu@gmail.com>
2017-10-02 00:01:04 +0700

    Con tro ham

--- Pointer/main.cpp
+++ Pointer/main.cpp
@@ -1,32 +1,64 @@
 #include <QCoreApplication>
 #include <iostream>
 
-using namespace std;
 
 int main(int argc, char *argv[])
 {
 //    QCoreApplication a(argc, argv);
-    int b =2;
-    int *a = &b;
-    cout << " &b:   " << &b << endl;
-    cout <<" &a:  " << &a << endl;
-    cout <<" a: " << a << endl;
-    cout <<" *a: Gia tri ma con tro a tro toi  " << *a << endl;
-//    cout <<"*a" << *a << endl;
+//    int b =2;
+//    int *a = &b;
+//    std::cout << " &b:   " << &b << std::endl;
+//    std::cout <<" &a:  " << &a << std::endl;
+//    std::cout <<" a: " << a << std::endl;
+//    std::cout <<" *a: Gia tri ma con tro a tro toi  " << *a << std::endl;
+////    cout <<"*a" << *a << endl;
+//    int fval =5;
+//    int sval =15;
+//    int *p1, *p2;
+//    p1 =&fval;
+//    p2 = &sval;
+//    *p1 =10;
+////    *p2 = *p1;
+//    p1 = p2;
+//    *p1 = 20;
+//    std::cout <<"sval 5 afet changed :  " << sval <<std:: endl;
+//    std::cout << "fval 15 after chaned :  " << fval << std::endl;
 
-    int fval =5;
-    int sval =15;
-    int *p1, *p2;
-    p1 =&fval;
-    p2 = &sval;
-    *p1 =10;
-//    *p2 = *p1;
-    p1 = p2;
-    *p1 = 20;
-    cout <<"sval 5 afet changed :  " << sval << endl;
-    cout << "fval 15 after chaned :  " << fval << endl;
+//    std::cout << " Bat dau con tro mang" << std::endl;
+// Con Tro cua con tro
 
-    return 0;//a.exec();
+    char a;
+    char *b;
+    char **c;
+    a = 'z';
+    b =&a;
+    c =&b;
+
+    // mang 2 chieu va con tro mang
+
+    int **matrix;
+    matrix = new int*[3];
+    for (int i=0; i<3; i++) {
+        matrix[i] = new int[3];
+    }
+    for (int i =0; i<3; i++) {
+        for (int j =0; j<3; j++) {
+            *(*(matrix+i)+j) = i+j;
+        }
+    }
+    //xuat ma tran
+    std::cout<< " ma tran la "<<std::endl;
+    for (int i =0; i<3; i++) {
+        for (int j =0; j<3; j++) {
+           std::cout<< *(*(matrix+i)+j) <<" ";
+        }
+        std::cout<< std::endl;
+    }
+
+    std::cout << "Con tro ham" << std::endl;
+
+
+    return 0;
 }
 
 
